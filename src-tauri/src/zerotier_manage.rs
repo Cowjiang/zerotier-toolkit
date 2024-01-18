e16aee73f215793cd286000055240d88c59c46ac
lvlizhe <1482084534@qq.com>
2024-01-18 09:31:46 +0800

    rename ZERO_TIER to ZEROTIER

--- src-tauri/src/zerotier_manage.rs
+++ src-tauri/src/zerotier_manage.rs
@@ -6,42 +6,42 @@ use lazy_static::lazy_static;
 use crate::windows_service_manage::WindowsServiceManage;
 
 lazy_static! {
-    static ref ZERO_TIER_SERVICE_NAME: String = {
+    static ref ZEROTIER_SERVICE_NAME: String = {
         String::from("ZeroTierOneService")
     };
-    static ref ZERO_TIER_SERVICE_MANAGE: WindowsServiceManage = {
-      WindowsServiceManage::new(ZERO_TIER_SERVICE_NAME.to_string())
+    static ref ZEROTIER_SERVICE_MANAGE: WindowsServiceManage = {
+      WindowsServiceManage::new(ZEROTIER_SERVICE_NAME.to_string())
     };
 }
 
 #[tauri::command]
 pub(crate) fn get_zerotier_services() -> String {
-    return ZERO_TIER_SERVICE_MANAGE.get_service_info();
+    return ZEROTIER_SERVICE_MANAGE.get_service_info();
 }
 
 #[tauri::command]
 pub(crate) fn get_zerotier_start_type() -> String {
-    return format!("{:?}", ZERO_TIER_SERVICE_MANAGE.get_start_type());
+    return format!("{:?}", ZEROTIER_SERVICE_MANAGE.get_start_type());
 }
 
 #[tauri::command]
 pub(crate) fn set_zerotier_start_type(start_type: String) {
-    ZERO_TIER_SERVICE_MANAGE.set_start_type(start_type);
+    ZEROTIER_SERVICE_MANAGE.set_start_type(start_type);
 }
 
 #[tauri::command]
 pub(crate) fn start_zerotier()  {
-    ZERO_TIER_SERVICE_MANAGE.start();
+    ZEROTIER_SERVICE_MANAGE.start();
 }
 
 #[tauri::command]
 pub(crate) fn stop_zerotier()  {
-    ZERO_TIER_SERVICE_MANAGE.stop();
+    ZEROTIER_SERVICE_MANAGE.stop();
 }
 
 #[tauri::command]
 pub(crate) fn get_zerotier_state() -> String {
-  return  format!("{:?}", ZERO_TIER_SERVICE_MANAGE.get_state());
+  return  format!("{:?}", ZEROTIER_SERVICE_MANAGE.get_state());
 }
 
 
