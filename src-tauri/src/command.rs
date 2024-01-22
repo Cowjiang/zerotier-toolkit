593991c665d02ad87df5ce371384c125802f303c
lvlizhe <1482084534@qq.com>
2024-01-22 15:39:45 +0800

    add handler `is_admin` to check current proccess has admin authority

--- src-tauri/src/command.rs
+++ src-tauri/src/command.rs
@@ -1,8 +1,5 @@
 use std::io;
 use std::process::{Command, Output};
-use std::str::from_utf8;
-
-use log::error;
 
 pub(crate) fn execute_cmd(cmds: Vec<String>) -> io::Result<Output> {
     let cmd_str: Vec<&str> = cmds.iter().map(|s| s.as_str()).collect();
@@ -12,25 +9,37 @@ pub(crate) fn execute_cmd(cmds: Vec<String>) -> io::Result<Output> {
 }
 
 pub(crate) fn parse_output(output: Vec<u8>) -> String {
-    let result = from_utf8(&*output);
-    return match result {
-        Ok(value) => value.to_string(),
-        Err(error) => {
-            error!("解析结果错误:{}", error);
-            "".to_string()
-        }
-    };
+    let result = String::from_utf8_lossy(&*output);
+    return result.to_string();
 }
+#[tauri::command]
 pub(crate) fn is_admin() -> String {
-    let output = execute_cmd(vec![String::from("net"),String::from("sessoin")]);
-    parse_output(output.unwrap().stdout)
+    let output = execute_cmd(vec![String::from("net session")]);
+    match output {
+        Ok(_value) => {
+            if _value.status.success() {
+                String::from("管理呀铁铁")
+            } else {
+                String::from("你不是管理捏")
+            }
+        }
+        Err(_error) => String::from("你不是管理捏"),
+    }
 }
 
 #[cfg(test)]
 mod tests {
 
-    use crate::command::{execute_cmd, is_admin, parse_output};
+    use log::info;
 
+    use crate::{
+        command::{execute_cmd, is_admin, parse_output},
+        logger::init_logger_with_level,
+    };
+
+    fn setup() {
+        init_logger_with_level(log::LevelFilter::Debug)
+    }
     #[test]
     fn test_execute_cmd() {
         let output = execute_cmd(vec![String::from("echo"), String::from("hello")]);
@@ -46,6 +55,7 @@ mod tests {
 
     #[test]
     fn test_is_admin() {
-        println!("{}", is_admin());
+        setup();
+        info!("{}", is_admin());
     }
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,33 +1,35 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use command::*;
+
 use crate::logger::init_logger;
 use crate::zerotier_manage::*;
 
-mod zerotier_manage;
-mod logger;
-mod windows_service_manage;
 mod command;
+mod logger;
 mod r;
-
+mod windows_service_manage;
+mod zerotier_manage;
 
 fn main() {
     init_logger();
     start_tauri();
 }
 
-
 fn start_tauri() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
+            // zerotier handler
             get_zerotier_services,
             get_zerotier_start_type,
             set_zerotier_start_type,
             start_zerotier,
             stop_zerotier,
-            get_zerotier_state
+            get_zerotier_state,
+            // other handlers
+            is_admin
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
-
