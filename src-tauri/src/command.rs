b7e1b7b6fece0e478714c36da6a1cbd209c4893d
lvlizhe <1482084534@qq.com>
2024-01-22 17:11:32 +0800

    add struce for invoke is_admin

--- src-tauri/src/command.rs
+++ src-tauri/src/command.rs
@@ -1,6 +1,8 @@
 use std::io;
 use std::process::{Command, Output};
 
+use crate::r::{self, R};
+
 pub(crate) fn execute_cmd(cmds: Vec<String>) -> io::Result<Output> {
     let cmd_str: Vec<&str> = cmds.iter().map(|s| s.as_str()).collect();
     let final_cmd = std::iter::once("/C").chain(cmd_str);
@@ -12,18 +14,19 @@ pub(crate) fn parse_output(output: Vec<u8>) -> String {
     let result = String::from_utf8_lossy(&*output);
     return result.to_string();
 }
+
 #[tauri::command]
 pub(crate) fn is_admin() -> String {
     let output = execute_cmd(vec![String::from("net session")]);
     match output {
         Ok(_value) => {
             if _value.status.success() {
-                String::from("管理呀铁铁")
+                r::success_json(String::from("管理呀铁铁"))
             } else {
-                String::from("你不是管理捏")
+                r::success_json(String::from("你不是管理捏"))
             }
         }
-        Err(_error) => String::from("你不是管理捏"),
+        Err(_error) => r::success_json(String::from("你不是管理捏")),
     }
 }
 
