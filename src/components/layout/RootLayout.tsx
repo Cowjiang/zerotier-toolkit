a881357b5965ef03682d14d715429fc766e46ae0
Cowjiang <cowjiang@163.com>
2024-04-30 14:26:16 +0800

    fix: redirect to homepage if url is invalid

--- src/components/layout/RootLayout.tsx
+++ src/components/layout/RootLayout.tsx
@@ -1,11 +1,19 @@
-import { Outlet, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
 
 import Splash from '../../pages/Splash.tsx'
 import { useAppStore } from '../../store/app.ts'
 
 function RootLayout() {
   const { showSplash } = useAppStore()
+  const navigate = useNavigate()
   const { pathname } = useLocation()
+
+  useEffect(() => {
+    if (pathname === '/') {
+      navigate('/home', { replace: true })
+    }
+  }, [pathname])
 
   return <div>{showSplash ? <Splash navigatePath={pathname} /> : <Outlet />}</div>
 }
