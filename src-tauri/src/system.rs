f57880325f181ef5d0377c88c2bac6bbbe062902
lvlizhe <1482084534@qq.com>
2024-06-22 01:33:39 +0800

    optimize: conditionally compile on restart_as_admin command

--- src-tauri/src/system.rs
+++ src-tauri/src/system.rs
@@ -7,36 +7,40 @@ use crate::{
     r::{fail_message_json, success_json},
 };
 
-
 #[tauri::command]
 pub(crate) fn restart_as_admin() -> String {
-    let current = env::current_exe().unwrap();
-    let exec_path = current.to_str();
-    if exec_path.is_none() {
-        return fail_message_json(String::from("can't not get current exe path"));
-    }
-    let exec_path = exec_path.unwrap();
-    let output = execute_cmd(vec![
-        String::from("powershell"),
-        String::from("-Command"),
-        String::from("Start-Process"),
-        String::from("-FilePath"),
-        String::from(exec_path),
-        String::from("-Verb"),
-        String::from("\"RunAs\""),
-        String::from("-WindowStyle"),
-        String::from("Hidden"),
-    ]);
-    return match output {
-        Ok(value) => {
-            let status = value.status;
-            if !status.success() {
-                return fail_message_json(String::from("resater fail.please restart manually"));
-            }
-            return success_json("success");
+    #[cfg(windows)]
+    {
+        let current = env::current_exe().unwrap();
+        let exec_path = current.to_str();
+        if exec_path.is_none() {
+            return fail_message_json(String::from("can't not get current exe path"));
         }
-        Err(err) => fail_message_json(err.to_string()),
-    };
+        let exec_path = exec_path.unwrap();
+        let output = execute_cmd(vec![
+            String::from("powershell"),
+            String::from("-Command"),
+            String::from("Start-Process"),
+            String::from("-FilePath"),
+            String::from(exec_path),
+            String::from("-Verb"),
+            String::from("\"RunAs\""),
+            String::from("-WindowStyle"),
+            String::from("Hidden"),
+        ]);
+        match output {
+            Ok(value) => {
+                let status = value.status;
+                if !status.success() {
+                    return fail_message_json(String::from("resater fail.please restart manually"));
+                }
+                return success_json("success");
+            }
+            Err(err) => fail_message_json(err.to_string()),
+        }
+    }
+    #[cfg(not(windows))]
+    fail_message_json(String::from("not support"));
 }
 
 #[tauri::command]
@@ -68,11 +72,11 @@ pub(crate) fn show_main_window(app_handler: AppHandle) -> String {
     return success_json("success");
 }
 
-
 #[cfg(test)]
 mod tests {
     use super::restart_as_admin;
 
+    #[cfg(windows)]
     #[test]
     fn test_restart_as_admin() {
         restart_as_admin();
