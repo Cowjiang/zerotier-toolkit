ef6de423d4edbe683aa5034faf20128123b8efd7
lvlizhe <1482084534@qq.com>
2024-08-12 18:17:05 +0800

    fix: update config in memory on cloned config

--- src-tauri/src/configurations/configurations_command.rs
+++ src-tauri/src/configurations/configurations_command.rs
@@ -1,7 +1,9 @@
-use std::collections::HashMap;
-use log::debug;
-use crate::configurations::configurations_service::get_configuration_context;
+use crate::configurations::configurations_service::{
+    get_configuration_context, put_configuration_context,
+};
 use crate::r::{fail_message_json, success_json};
+use log::debug;
+use std::collections::HashMap;
 
 #[tauri::command]
 pub fn get_configurations(name: String) -> String {
@@ -9,20 +11,13 @@ pub fn get_configurations(name: String) -> String {
     return success_json(context.unwrap().get_configs());
 }
 
-
 #[tauri::command]
 pub fn put_configurations(name: String, payload: String) -> String {
     debug!("accept command:payload[{payload}]");
-    let config: HashMap<String, String> =
+    let configs: HashMap<String, String> =
         serde_json::from_str(&*payload).expect("value type of configuration is String only");
-    let context = get_configuration_context(&name);
-    if context.is_none() {
-        return fail_message_json(format!("configurations {} is not found", name).as_str());
+    match put_configuration_context(&name, configs) {
+        Ok(..) => success_json(""),
+        Err(e) => fail_message_json(&e),
     }
-    let mut context = context.unwrap();
-    for (key, value) in &config {
-        context.put_config(key.clone(), value.clone());
-    }
-    context.store_config();
-    success_json("")
 }
--- src-tauri/src/configurations/configurations_service.rs
+++ src-tauri/src/configurations/configurations_service.rs
@@ -1,20 +1,24 @@
+use std::collections::HashMap;
+
 use lazy_static::lazy_static;
 use parking_lot::RwLock;
 use tauri::AppHandle;
-use crate::configurations::configuration_base::ConfigurationContext;
-lazy_static! {
-    static ref CONFIGURATION_GROUPS:RwLock<Vec<ConfigurationContext>> = RwLock::new(Vec::new());
-}
 
+use crate::configurations::configuration_base::ConfigurationContext;
+
+lazy_static! {
+    static ref CONFIGURATION_GROUPS: RwLock<Vec<ConfigurationContext>> = RwLock::new(Vec::new());
+}
 
 pub fn init_configuration_context(app_handle: &AppHandle) {
     let mut configuration_groups = CONFIGURATION_GROUPS.write();
-    let system_config_context = crate::configurations::system_configurations::init_context(&app_handle);
+    let system_config_context =
+        crate::configurations::system_configurations::init_context(&app_handle);
     configuration_groups.push(system_config_context);
-    let auth_config_context = crate::configurations::zerotier_configurations::init_context(&app_handle);
+    let auth_config_context =
+        crate::configurations::zerotier_configurations::init_context(&app_handle);
     configuration_groups.push(auth_config_context);
 }
-
 
 pub fn get_configuration_context(name: &String) -> Option<ConfigurationContext> {
     let configuration_groups = CONFIGURATION_GROUPS.read();
@@ -26,7 +30,26 @@ pub fn get_configuration_context(name: &String) -> Option<ConfigurationContext> 
     None
 }
 
+pub fn put_configuration_context(
+    name: &String,
+    configs: HashMap<String, String>,
+) -> Result<(), String> {
+    let mut configuration_groups = CONFIGURATION_GROUPS.write();
+    for context in configuration_groups.iter_mut() {
+        if context.name() == name {
+            for (key, value) in &configs {
+                context.put_config(key.clone(), value.clone());
+            }
+            context.store_config();
+            return Ok(());
+        }
+    }
+    Err(format!("configuration [{name}] not found"))
+}
+
 pub fn backup_all() {
     let mut configuration_groups = CONFIGURATION_GROUPS.write();
-    configuration_groups.iter_mut().for_each(|v| v.store_config_bak());
+    configuration_groups
+        .iter_mut()
+        .for_each(|v| v.store_config_bak());
 }
