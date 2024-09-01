34a4fb17c4d331cc1dcedfc02fc03a11c00be588
Cowjiang <cowjiang@163.com>
2024-09-01 19:12:09 +0800

    feat: add powered-by

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Divider, Image, Listbox, ListboxItem, ListboxSection } from '@nextui-org/react'
+import { Avatar, Divider, Image, Link, Listbox, ListboxItem, ListboxSection } from '@nextui-org/react'
 import { useState } from 'react'
 
 import { BugIcon, GithubIcon, TagsIcon } from '../components/base/Icon.tsx'
@@ -14,6 +14,24 @@ const developers = [
     name: 'Agitator',
     userId: 'Code-Agitator',
     email: '1482084534@qq.com',
+  },
+]
+
+const githubLinks = [
+  {
+    title: 'Check For Updates',
+    description: 'Latest releases',
+    startContent: <TagsIcon width={18} />,
+  },
+  {
+    title: 'Github Repository',
+    description: 'Source code',
+    startContent: <GithubIcon width={18} />,
+  },
+  {
+    title: 'Report Issues',
+    description: 'Report issues',
+    startContent: <BugIcon width={18} />,
   },
 ]
 
@@ -38,27 +56,24 @@ function About() {
       </div>
       <Divider className="mt-8 mb-4" />
       <div className="flex gap-6">
-        <Listbox variant="flat">
+        <Listbox variant="flat" aria-label="Github links">
           <ListboxSection title="Github">
-            <ListboxItem key="releases" description="Latest Releases" startContent={<TagsIcon width={18} />}>
-              Check For Updates
-            </ListboxItem>
-            <ListboxItem key="repo" description="Source Code" startContent={<GithubIcon width={18} />}>
-              Github Repository
-            </ListboxItem>
-            <ListboxItem
-              key="issue"
-              description="Generate a pre-filled Github issue"
-              startContent={<BugIcon width={18} />}
-            >
-              Report an Issue
-            </ListboxItem>
+            {githubLinks.map((link) => (
+              <ListboxItem
+                key={link.title}
+                aria-label={link.title}
+                description={link.description}
+                startContent={link.startContent}
+              >
+                {link.title}
+              </ListboxItem>
+            ))}
           </ListboxSection>
         </Listbox>
-        <Listbox variant="flat">
+        <Listbox variant="flat" aria-label="Developers">
           <ListboxSection title="Developers">
             {developers.map((user) => (
-              <ListboxItem key={user.userId}>
+              <ListboxItem key={user.userId} aria-label={user.name}>
                 <div className="flex gap-2 items-center">
                   <Avatar
                     className="flex-shrink-0"
@@ -77,6 +92,12 @@ function About() {
             ))}
           </ListboxSection>
         </Listbox>
+      </div>
+      <div className="mt-4 px-2 flex flex-col justify-center">
+        <p className="text-sm text-foreground-400">
+          Powered by <Link size="sm">Tauri</Link> / <Link size="sm">NextUI</Link> / <Link size="sm">Flaticon</Link>,
+          following MIT License
+        </p>
       </div>
     </div>
   )
